3003f4f115b87c7a50877f0512a1eca08abaecf0
sumit mishra <mishrasumit042@gmail.com>
2025-02-15 18:49:48 +0530

    Update Maximum Subarray.js

--- Maximum Subarray.js
+++ Maximum Subarray.js
@@ -4,7 +4,9 @@ function maxSubarraySum(nums) {
 
     for (let i = 1; i < nums.length; i++) {
         // If currentSum + nums[i] is greater, it means the subarray benefits from adding nums[i].
-        // If nums[i] is greater, it means the previous sum was a burden, so we restart the subarray from nums[i].
+        // If nums[i] is greater than currentSum + nums[i], it means the previous sum was a burden, so we restart the subarray from nums[i].
+        // -1,-2,6,3 => max[6, 6-3] => 6 and hence discard the previous sum and move on with 6 as the new current sum
+        
         currentSum = Math.max(nums[i], currentSum + nums[i]);
         maxSum = Math.max(maxSum, currentSum); // Update max sum if needed
     }
